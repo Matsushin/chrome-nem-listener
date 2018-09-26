26813992405659eb64868335d6d20ef7c9d21ea7
matsushin11 <matsu1983@gmail.com>
2018-09-26 22:18:18 +0900

    監視対象アドレスを保管するように

--- src/js/background.js
+++ src/js/background.js
@@ -3,33 +3,49 @@ import '../img/icon-34.png'
 
 import {Address, ConfirmedTransactionListener, NEMLibrary, NetworkTypes} from "nem-library";
 NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
-const address = "TBIMTT24HUZ7BWUO2ZUQTUYV24MYGRIZTMYSRBDJ";
-const confirmedTransactionListener = new ConfirmedTransactionListener([
-{
-    domain: '23.228.67.85'
-},
-]).given(new Address(address));
-confirmedTransactionListener.subscribe(res => {
-    const signerAddress = res.signer.address.value;
-    if (address == res.recipient.value) {
-        let amount = 0;
-        let mosaicName = '';
-        if (res.mosaics == undefined) {
-            // XEM
-            amount = res._xem.quantity / 1000000;
-            mosaicName = 'xem';
-        } else {
-            // Mosaic
-            const mosaic = res._mosaics[0]
-            amount = mosaic.quantity / 100;
-            mosaicName = `${mosaic.mosaicId.namespaceId}:${mosaic.mosaicId.name}`;
-        }
-        const title = `${amount}${mosaicName} 受け取りました！`
-        const options = {
-            body: `送金元：${signerAddress}`
-        }
-        new Notification(title, options);
-    } 
-}, err => {
-    console.log(err);
-});
+
+const address = localStorage.getItem("address");
+if (address != '') {
+    startNotification(address);
+}
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        startNotification(request.address);
+        sendResponse({
+            msg: "通知を開始しました!",
+            address: request.address
+        });
+    }
+);
+
+function startNotification(address) {
+    const confirmedTransactionListener = new ConfirmedTransactionListener([
+    {
+        domain: '23.228.67.85'
+    },
+    ]).given(new Address(address));
+    confirmedTransactionListener.subscribe(res => {
+        const signerAddress = res.signer.address.value;
+        if (address == res.recipient.value) {
+            let amount = 0;
+            let mosaicName = '';
+            if (res.mosaics == undefined) {
+                // XEM
+                amount = res._xem.quantity / 1000000;
+                mosaicName = 'xem';
+            } else {
+                // Mosaic
+                const mosaic = res._mosaics[0]
+                amount = mosaic.quantity / 100;
+                mosaicName = `${mosaic.mosaicId.namespaceId}:${mosaic.mosaicId.name}`;
+            }
+            const title = `${amount}${mosaicName} 受け取りました！`
+            const options = {
+                body: `送金元：${signerAddress}`
+            }
+            new Notification(title, options);
+        } 
+    }, err => {
+        console.log(err);
+    });
+}
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,4 +1,19 @@
 import "../css/popup.css";
 import hello from "./popup/example";
 
+let addressForm = document.getElementById("addressForm");
+addressForm.value = localStorage.getItem("address");
+
+let button = document.getElementById("startButton");
+button.addEventListener('click', function() {
+    const address = addressForm.value;
+    chrome.runtime.sendMessage({
+        address: address
+    },
+    function(response) {
+        alert(response.msg);
+        localStorage.setItem("address", response.address);
+    });
+});
+
 //hello();
